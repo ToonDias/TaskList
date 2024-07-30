ac925f023d1da76f812a060a34a64d819ba9c853
aylton <aylton@colegioexpansivo.com>
2024-07-29 23:52:44 -0300

    resultado do final do dia

--- tasks/taskscontroller.js
+++ tasks/taskscontroller.js
@@ -1,5 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const Task = require("./Task");
+const { where } = require("sequelize");
 
+router.get("/admin/tanks", (req, res) => {
+    Task.findAll().then( tasks => {
+        if(tasks != undefined){
+            res.render("admin/tasks/index", {tasks: tasks});
+        }
+    }).catch( error => {
+        res.redirect("/");
+    });
+});
+
+router.get("/admin/tasks/new", (req, res) => {
+    res.render("admin/tanks/create");
+});
+
+router.post("admin/tasks/save", (req, res) => {
+    var id = req.body.id;
+    var nome = req.body.nome;
+    var status = req.body.status;
+    var responsavel = req.body.respnsalvel;
+    res.json({id, nome, status,responsavel});    
+});
+
+router.get("/admin/tasks/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Task.findByPk(id).then( task =>{
+        if(task != undefined){
+            res.render("admin/tanks/update", {task: task});
+        }else{
+            res.redirect("/admin/tasks");
+        }
+    }).catch( error =>{ 
+        res.redirect("/admin/tasks");
+    });
+});
+
+router.post("/admin/tasks/saveupdate", (req, res) =>{
+    var id = req.body.id;
+    var nome = req.body.nome;
+    var status = req.body.status;
+    var responsavel = req.body.respnsalvel;
+    res.json({id, nome, status,responsavel}); 
+});
+
+router.post("/admin/tasks/delete", (req, res) => {
+    var id = req.body.id;
+    Task.destory({where: {id: id}});
+});
 
 module.exports = router;
